a3bedcd26af643dd3cdad0d89f6da0fce8cd0a4e
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-04-04 19:38:30 +0200

    nothing really

--- index.js
+++ index.js
@@ -22,17 +22,12 @@ var vueButton = new Vue({
     methods: {
         start: function (){
 
-            if(textBox.textBox1 == CTask){console.log("success")}
-
             tmp =  Math.floor(Math.random() * 11);
             tmp2 =  Math.floor(Math.random() * 11);
             CTask = tmp * tmp2
 
             this.message = "Nächste Aufgabe!"
             textDisplay.message = "Was ist "+tmp+" * "+tmp2+"?"
-            if(textBox.textBox1 == CTask){
-                textDisplay.message = "Gut gemacht, du hast die Aufgabe richtig gelöst!"
-            } // end if
         } // end start method
     } // End methods
 })
@@ -45,7 +40,6 @@ var textBox = new Vue({
     },
     methods: {
         check: function () {
-            
         } // End function check
     } // End methods
 })
