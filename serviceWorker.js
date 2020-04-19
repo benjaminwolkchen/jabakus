d5a32a872686249f1becec08ff96f76ff12f3cf9
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-04-19 15:31:34 +0200

    PWA

---
+++ serviceWorker.js
@@ -0,0 +1,40 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/main.min.css",
+  "/minus/index.min.js",
+  "/dividing/index.min.js",
+  "/plus/index.min.js",
+  "/times/index.min.js",
+  // Style Sheets
+  "/minus/index.min.css",
+  "/dividing/index.min.css",
+  "/plus/index.min.css",
+  "/times/index.min.css",
+  // App.js itself
+  "/js/app.js",
+  // vue.js
+  "https://cdn.jsdelivr.net/npm/vue",
+  // vue.cookie.js
+  "https://unpkg.com/vue-cookies@1.7.0/vue-cookies.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+// Fetch Cache
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
+
