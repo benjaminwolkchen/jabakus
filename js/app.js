ec9135a420277969e49786d379b4d15a3db38fdf
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-04-19 19:26:34 +0200

    PWA fixes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/1o1/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
-  
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
