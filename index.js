27cb896dbdf15de340fbd36ffd4bbbad327ad009
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-04-25 11:52:02 +0200

    Added translation support

---
+++ index.js
@@ -0,0 +1,9 @@
+var vueComponent = new Vue({ // for translation
+    el: '#html',
+    data: {
+        title: "Wähle eine Rechenart!",
+        plus: "Plus",
+        minus: "Minus",
+        times: "Mal",
+        dividing: "Geteilt"
+    }
