b8515aaa00dd7d78048f8dba13f9eaf9df029bfe
Benjamin Wölkchen <42138517+benjaminwolkchen@users.noreply.github.com>
2020-04-30 11:44:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
-echo "ENVCHECK"
-echo $ENVCHECK
-
-echo "trigger jabakustrap deploy"
-curl -X POST -d {} https://api.netlify.com/build_hooks/$DEPLOYKEY
-
-echo ""
+echo "     ▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄   ▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ 
+    █   █       █  ▄    █       █   █ █ █  █ █  █       █
+    █   █   ▄   █ █▄█   █   ▄   █   █▄█ █  █ █  █  ▄▄▄▄▄█
+ ▄  █   █  █▄█  █       █  █▄█  █      ▄█  █▄█  █ █▄▄▄▄▄ 
+█ █▄█   █       █  ▄   ██       █     █▄█       █▄▄▄▄▄  █
+█       █   ▄   █ █▄█   █   ▄   █    ▄  █       █▄▄▄▄▄█ █
+█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄█ █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█
+"
 echo "finished"
 
