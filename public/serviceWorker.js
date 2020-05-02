ccc8015aaff52db1f5fbf6ece7f042ff3f5eafc2
Benjamin Wölkchen <42138517+benjaminwolkchen@users.noreply.github.com>
2020-05-02 11:32:55 +0200

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,13 +1,13 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
-  "translation.js",
-  "index.html",
-  "serviceWorker.js",
-  "manifest.json",
-  "main.min.css",
-  "favicon.png",
-  "js/app.js",
-  "js/copy.js",
+  "/translation.js",
+  "/index.html",
+  "/serviceWorker.js",
+  "/manifest.json",
+  "/main.min.css",
+  "/favicon.png",
+  "/js/app.js",
+  "/js/copy.js",
   // jQuery
   "https://code.jquery.com/jquery-3.4.1.slim.min.js",
   // Bootstrap
@@ -16,6 +16,9 @@ const assets = [
   "https://cdn.jsdelivr.net/npm/vue",
   // vue.cookie.js
   "https://unpkg.com/vue-cookies@1.7.0/vue-cookies.js",
+  "game/index.html",
+  "index.min.js",
+  "main.min.css"
 ]
 
 self.addEventListener("install", installEvent => {
