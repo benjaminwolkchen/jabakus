cc47132535cfd68f5a8dc8d3030393c0091bdf4f
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-13 20:12:19 +0200

    test json

--- assets/lang/index.js
+++ assets/lang/index.js
@@ -1,15 +1,16 @@
 var app = new Vue({
     el: '#wow',
     data: {
-        LocaleStrings: {},
-        url: "https://raw.githubusercontent.com/jabakus/jabakus/rewrite/assets/lang/de.json"
-    },
+        url: "https://cdn.jsdelivr.net/gh/jabakus/jabakus@rewrite/assets/lang/de.json",
+
+        loc: {},
+        },
     created(){
         fetch(this.url)
         .then((resp) => resp.json()) // Transform the data into json
-        .then(function(data) {
-          this.LocaleStrings = data;
-          console.log(this.LocaleStrings)
+        .then(function(response) {
+          this.loc = JSON.parse(response);
+          console.log(this.loc)
           })
     }
 });
