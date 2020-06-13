c62e808a86db20a40ae9597a9afe3d8e23a970b7
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-13 18:40:57 +0200

    dev: changed to less-caching json source

--- assets/lang/index.js
+++ assets/lang/index.js
@@ -4,7 +4,7 @@ const app = new Vue({
     },
 
     created() {
-        fetch('https://cdn.jsdelivr.net/gh/jabakus/jabakus@rewrite/assets/lang/de.json')
+        fetch('https://raw.githubusercontent.com/jabakus/jabakus/rewrite/assets/lang/de.json')
         .then(response => response.json())
         .then(json => {
             this.locale = json.locale
