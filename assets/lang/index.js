bfad078723f3a284d8c78e0c13ca098f1473fe12
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-13 20:21:40 +0200

    dont use vue

--- assets/lang/index.js
+++ assets/lang/index.js
@@ -1,16 +1,7 @@
-var app = new Vue({
-    el: '#wow',
-    data: {
-        url: "https://cdn.jsdelivr.net/gh/jabakus/jabakus@rewrite/assets/lang/de.json",
-
-        loc: {},
-        },
-    created(){
-        fetch(this.url)
-        .then((resp) => resp.json()) // Transform the data into json
-        .then(function(response) {
-          this.loc = JSON.parse(response);
-          console.log(this.loc)
-          })
-    }
-});
+var data
+var url = "https://raw.githubusercontent.com/jabakus/jabakus/rewrite/assets/lang/de.json"
+fetch(url)
+.then(response => response.json()) // Transform the data into json
+.then(
+    json => data = json 
+)
