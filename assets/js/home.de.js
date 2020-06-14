b4f20d1cab4889a86640acf5d9c3be5df163d390
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-14 13:07:52 +0200

    updated json

---
+++ assets/js/home.de.js
@@ -0,0 +1,15 @@
+var data // Contains the fetched json language data.
+
+
+fetch("https://raw.githubusercontent.com/jabakus/jabakus/rewrite/assets/lang/de.json")
+    .then(response => response.json()) // Transform the data into json
+    .then(
+    json => VueLang.lang = json
+)
+
+var VueLang = new Vue({
+    el:"#VueMain",
+    data:{
+        lang: data,
+    }
+})
--- assets/lang/index.js
+++
@@ -1,7 +0,0 @@
-var data
-var url = "https://raw.githubusercontent.com/jabakus/jabakus/rewrite/assets/lang/de.json"
-fetch(url)
-.then(response => response.json()) // Transform the data into json
-.then(
-    json => data = json 
-)
