6b9282310554bcdffff98694aac1143dc2059fa7
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-24 14:46:15 +0200

    Vue: Layout and Instance

--- assets/js/game/main.js
+++ assets/js/game/main.js
@@ -1,6 +1,13 @@
 var mainGame = new Vue({
     el: '#app',
     data: {
-        message: "Starte das Spiel!"
+        message: "Starte das Spiel",
+        UserInput: 0,
     },
+    methods:{
+        start: function(){
+            // Change Button Text
+            mainGame.message = "Nächste Aufgabe"
+        } // END start method
+    }
 })
