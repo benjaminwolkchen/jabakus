077a8309750fb5429991a32e455eb9b320a209ec
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-06-24 19:57:55 +0200

    js made progress

--- assets/js/game/main.js
+++ assets/js/game/main.js
@@ -1,13 +1,57 @@
-var mainGame = new Vue({
-    el: '#app',
-    data: {
-        message: "Starte das Spiel",
-        UserInput: 0,
-    },
-    methods:{
-        start: function(){
-            // Change Button Text
-            mainGame.message = "Nächste Aufgabe"
-        } // END start method
-    }
-})
+if(conf.mode == "plus"){
+    var mainGame = new Vue({
+        el: '#app',
+        data: {
+            message: "Starte das Spiel",
+            UserInput: 0,
+        },
+        methods:{
+            start: function(){
+                // Change Button Text
+                mainGame.message = "Nächste Aufgabe"
+            } // END start method
+        }
+    })
+}else if(conf.mode == "minus"){
+    var mainGame = new Vue({
+        el: '#app',
+        data: {
+            message: "Starte das Spiel",
+            UserInput: 0,
+        },
+        methods:{
+            start: function(){
+                // Change Button Text
+                mainGame.message = "Nächste Aufgabe"
+            } // END start method
+        }
+    })
+}else if(conf.mode == "times"){
+    var mainGame = new Vue({
+        el: '#app',
+        data: {
+            message: "Starte das Spiel",
+            UserInput: 0,
+        },
+        methods:{
+            start: function(){
+                // Change Button Text
+                mainGame.message = "Nächste Aufgabe"
+            } // END start method
+        }
+    })
+}else if(conf.mode == "dividing"){
+    var mainGame = new Vue({
+        el: '#app',
+        data: {
+            message: "Starte das Spiel",
+            UserInput: 0,
+        },
+        methods:{
+            start: function(){
+                // Change Button Text
+                mainGame.message = "Nächste Aufgabe"
+            } // END start method
+        }
+    })
+}
