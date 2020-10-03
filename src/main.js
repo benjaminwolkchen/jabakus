785ce8659506c0fe8bb540c9f362bd7b4237877d
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-10-03 18:33:11 +0200

    initial router config

--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-import Plus from './pages/plus.vue'
-import Minus from './pages/minus.vue'
-import Times from './pages/times.vue'
-import Dividing from './pages/dividing.vue'
-
-if(window.location.pathname == "/"){
-    createApp(App).mount('#app')
-}else if(window.location.pathname == "/plus"){
-    createApp(Plus).mount('#app')
-}else if(window.location.pathname == "/minus"){
-    createApp(Minus).mount('#app')
-}else if(window.location.pathname == "/times"){
-    createApp(Times).mount('#app')
-}else if(window.location.pathname == "/dividing"){
-    createApp(Dividing).mount('#app')
-}
+createApp(App)
+  .use(router)
+  .mount("#app");
