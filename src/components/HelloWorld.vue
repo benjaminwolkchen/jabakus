8edc20289e8bfadbd2764818f1d7a8ff553f25a9
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-10-04 15:41:12 +0200

    added deploys by netlify button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <router-link to="/plus">Plus</router-link> <br /><br />
-    <router-link to="/minus">Minus</router-link> <br /><br />
-    <router-link to="/times">Mal</router-link> <br /><br />
-    <router-link to="/dividing">Geteilt</router-link> <br /><br />
+    <router-link to="/plus" class="btn">Plus</router-link> <br /><br />
+    <router-link to="/minus" class="btn">Minus</router-link> <br /><br />
+    <router-link to="/times" class="btn">Mal</router-link> <br /><br />
+    <router-link to="/dividing" class="btn">Geteilt</router-link> <br /><br />
   </div>
 </template>
 
@@ -31,6 +31,13 @@ li {
   margin: 0 10px;
 }
 a {
+  background: #2c3e50;
+  border: none;
   color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 </style>
